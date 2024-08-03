acffb8515b39e5811f7c0b6b03d351dfc53a39da
melodysium <melodysium@gmail.com>
2024-08-02 21:11:03 -0400

    feat: render music glyphs like sharps and flats

--- renderer/music_text.py
+++ renderer/music_text.py
@@ -1,7 +1,34 @@
 from music.music_constants import Note
-from manim import Text
+from manim import Tex, Text, Scene, LEFT, RIGHT, Create, TexTemplate
+from constants import SKIP_LATEX
 
-class TextNote(Text):
-  def __init__(self, note: Note, **kwargs):
-    Text.__init__(self, note.display, **kwargs)
-    self.note = note
+myTemplate = TexTemplate()
+myTemplate.add_to_preamble(r"""\usepackage{musicography} 
+\usepackage{newunicodechar}
+\newunicodechar{𝄫}{\musDoubleFlat} 
+\newunicodechar{♭}{\musFlat} 
+\newunicodechar{♮}{\musNatural} 
+\newunicodechar{♯}{\musSharp} 
+\newunicodechar{𝄪}{\musDoubleSharp}""")
+
+print(f"{SKIP_LATEX=}")
+if SKIP_LATEX:
+  class TextNote(Text):
+    def __init__(self, note: Note, **kwargs):
+      Text.__init__(self, note.display_portable, **kwargs)
+      self.note = note
+else:
+  class TextNote(Tex):
+    def __init__(self, note: Note, **kwargs):
+      Tex.__init__(self, note.display, tex_template=myTemplate, **kwargs)
+      self.note = note
+
+
+class test(Scene):
+  def construct(self):
+    self.wait(0.2)
+    note_C = TextNote(Note.C)
+    note_Cb = TextNote(Note.Cb).shift(2 * LEFT)
+    note_Cs = TextNote(Note.Cs).shift(2 * RIGHT)
+    self.play(Create(note_Cb), Create(note_C), Create(note_Cs), run_time=2)
+    self.wait(1)
