bd5e2571777c5a4eb7c1a8972413d5f0b8c3c436
melodysium <melodysium@gmail.com>
2025-09-20 23:34:35 -0400

    feat: clean up radius & pitches_per_step, add a few more animation properties on Circle12Notes

--- godot-animator/src/component/clrcle_12_notes.gd
+++ godot-animator/src/component/clrcle_12_notes.gd
@@ -1,6 +1,16 @@
 @tool
 
+class_name Circle12Notes
+
 extends Node2D
+
+## TODO: make these configurable?
+## Font size for pitch names
+const BASE_PITCH_NAME_FONT_SIZE := 12
+## Circle radius for pitch circles
+const BASE_PITCH_CIRCLE_RADIUS := 0.2
+
+@export_group("setup options")
 
 ## Radius for main circle / pitch names 
 @export_range(0, 100, 1.0, "or_greater") var radius := 50.0:
@@ -8,11 +18,54 @@ extends Node2D
 		assert(_background_circle_node != null)
 		_background_circle_node.radius = val
 		radius = val
-# TODO: eliminate this, just use Godot native object scaling
+		_move_pitch_nodes()
 
 
 ## Number of steps between each pitch
-@export_range(1, 11, 1.0) var pitches_per_step := 1
+@export_range(1, 11, 1.0) var pitches_per_step := 1:
+	set(val):
+		if ![1, 5, 7, 11].has(val):
+			print_rich("[color=red]WARNING: Circle12Notes currently only supports pitches_per_step values of [1, 5, 7, 11], but you picked: %s. Ignoring.[/color]" % val)
+			return
+		pitches_per_step = val
+		_move_pitch_nodes()
+
+## First pitch class at the "top" of the node. 0 = C, 1 = Db, ... 11 = B
+@export_range(0, 11, 1.0) var first_pitch_class := 0:
+	set(val):
+		first_pitch_class = val
+		_move_pitch_nodes()
+
+
+@export_group("animation controls")
+
+## Rotate angle (keeps note text oriented upwards)
+@export_range(0.0, TAU, 0.01) var rotate_angle := 0.0:
+	set(val):
+		# if < 0 or >= TAU (radians), cycle back within that range
+		val = fmod(val, TAU)
+		rotate_angle = val
+		_move_pitch_nodes()
+
+## Colors used for pitch highlight circles. Start transparent.
+@export var pitch_circle_colors: Array[Color] = Array(Utils.fill_array(12, Color.TRANSPARENT), TYPE_COLOR, "", null):
+	set(val):
+		# TODO: This property seems unable to be reset at all in the editor. Maybe add a manual "reset" button? Maybe investigate resetting arrays in properties?
+		print("pitch_circle_colors.set(val=%s), oldval=%s" % [val, pitch_circle_colors])
+		if val == null:
+			print_rich("[color=red]WARNING: pitch_circle_colors requires a non-null array, but null provided. Ignoring.[/color]" % val.size())
+			return
+		if val.size() != 12:
+			print_rich("[color=red]WARNING: pitch_circle_colors requires an array of 12 elements, but provided one has size()=%s. Ignoring.[/color]" % val.size())
+			return
+		if val.any(func(elem: Variant) -> bool: return elem is not Color):
+			print_rich("[color=red]WARNING: pitch_circle_colors requires an array of Color elements, but provided one some non-Color elements. Ignoring.[/color]" % val.size())
+			return
+		pitch_circle_colors = val
+		if self._pitch_circle_nodes != null and self._pitch_circle_nodes.size() == 12:
+			_configure_pitch_circle_nodes()
+		
+
 
 @export_group("internal links")
 ## Background ring underneath pitches
@@ -22,8 +75,12 @@ extends Node2D
 ## Parent node for dynamically-added pitch circles
 @export var _pitch_circles_parent_node: Node2D
 
-const BASE_PITCH_NAME_FONT_SIZE := 12
-const BASE_PITCH_CIRCLE_RADIUS := 0.2
+
+## Mapping of pitch_class to pitch labels
+var _pitch_text_nodes: Dictionary[int, Text2D] = {}
+
+## Mapping of pitch_class to pitch labels
+var _pitch_circle_nodes: Dictionary[int, Circle2D] = {}
 
 class PitchInfo:
 	var pitch_class: int
@@ -32,22 +89,38 @@ class PitchInfo:
 	func _init(pitch_class: int, pos: Vector2) -> void:
 		self.pitch_class = pitch_class
 		self.pos = pos
+	
+	func _to_string() -> String:
+		return "PitchInfo[pcls=%s, pos=%s]" % [self.pitch_class, self.pos]
 
-var circle_start := Vector2.UP * radius
 const ONE_STEP_ANGLE := TAU / 12
+
+func _circle_start() -> Vector2:
+	return Vector2.UP * self.radius
 
 func _list_positions() -> Array[PitchInfo]:
 	var positions: Array[PitchInfo] = []
-	print("pitches_per_step=%s" % pitches_per_step)
 	for i: int in range(12):
-		var step := (i * pitches_per_step) % 12
-		print("step=%s" % step)
-		#print("transform=%s" % transform)
-		var pos := circle_start.rotated(ONE_STEP_ANGLE * i)
+		var step := (i * self.pitches_per_step + self.first_pitch_class) % 12
+		var pos := _circle_start().rotated(ONE_STEP_ANGLE * i + self.rotate_angle)
 		positions.append(PitchInfo.new(step, pos))
+	print_verbose("_list_positions(). pitches_per_step=%s; first_pitch_class=%s; rotate_angle=%s; positions=%s" % [pitches_per_step, first_pitch_class, rotate_angle, positions])
 	return positions
 
 
+func _move_pitch_nodes() -> void:
+	for pitch_info: PitchInfo in _list_positions():
+		_pitch_text_nodes[pitch_info.pitch_class].position = pitch_info.pos
+		_pitch_circle_nodes[pitch_info.pitch_class].position = pitch_info.pos
+
+func _configure_pitch_circle_nodes() -> void:
+	print_verbose("start _configire_pitch_circle_nodes(). _pitch_circle_nodes=%s, pitch_circle_colors=%s" % [_pitch_circle_nodes, pitch_circle_colors])
+	for i in range(12):
+		_pitch_circle_nodes[i].color = pitch_circle_colors[i]
+
+func _configure_pitch_text_nodes() -> void:
+	for i in range(12):
+		_pitch_text_nodes[i].text = Utils.Pitch.pitchclass_to_pitchname(i)
 
 func _ready() -> void:
 
@@ -55,8 +128,8 @@ func _ready() -> void:
 	for pitch_info: PitchInfo in _list_positions():
 		
 		# Pitch text
-		var pitch_name = Utils.Pitch.pitchclass_to_pitchname(pitch_info.pitch_class)
-		var text = Text2D.new(
+		var pitch_name := Utils.Pitch.pitchclass_to_pitchname(pitch_info.pitch_class)
+		var text := Text2D.new(
 			Utils.DEFAULT_FONT,
 			pitch_name,
 			HORIZONTAL_ALIGNMENT_CENTER,
@@ -64,18 +137,23 @@ func _ready() -> void:
 			5000,
 			BASE_PITCH_NAME_FONT_SIZE
 		)
-		_pitch_text_parent_node.add_child(text, true) # TODO: disable force_readable_name
-		text.position = pitch_info.pos
+		_pitch_text_nodes[pitch_info.pitch_class] = text
+		_pitch_text_parent_node.add_child(text, true)
+		#text.position = pitch_info.pos
 		#print("PitchInfo(pitch_class=%s, pos=%s)" % [pitch_info.pitch_class, pitch_info.pos])
 		#print("text.global_position=%s" % text.global_position)
 		
 		# Pitch circle
-		var circle = Circle2D.new(
+		var circle := Circle2D.new(
 			self.radius * BASE_PITCH_CIRCLE_RADIUS,
 			Color.WHITE,
 			false,
 			self.radius * .02
 		)
+		_pitch_circle_nodes[pitch_info.pitch_class] = circle
 		_pitch_circles_parent_node.add_child(circle, true)
-		circle.position = pitch_info.pos
-		circle.visible = false # start off by default
+		#circle.visible = false # start off by default
+		#circle.position = pitch_info.pos
+	self._move_pitch_nodes()
+	self._configure_pitch_text_nodes()
+	self._configure_pitch_circle_nodes()
