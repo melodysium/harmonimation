91f84d8324e790c0a11f7c7c84564288bddfe575
melodysium <melodysium@gmail.com>
2025-11-08 23:11:39 -0500

    feat: cli args to stop after certain stage of python script

--- renderer/main.py
+++ renderer/main.py
@@ -1,6 +1,7 @@
 # standard lib
 import argparse
 import logging
+from enum import Enum
 
 # 3rd party
 import pyjson5
@@ -31,6 +32,16 @@ logger.debug(f"Version info: {sys.version_info}")
 # or at least some folders in the main space here
 
 
+class ProcessStage(Enum):
+    parse_score = "parse_score"
+    timing = "timing"
+    animate = "animate"
+
+    def __str__(self):
+        return self.name
+
+
+
 def parse_args():
 
     def range_str(txt: str) -> tuple[float, float]:
@@ -57,10 +68,11 @@ def parse_args():
         type=argparse.FileType('r', encoding='UTF-8'),
         help="musicxml file to render music from",
     )
-    parser.add_argument(
+    hrmn_file_arg_def = parser.add_argument(
         "harmonimation_file",
         type=argparse.FileType('r', encoding="UTF-8"),
         help="harmonimation.json file to configure layout",
+        nargs='?',
     )
     parser.add_argument(
         "-b",
@@ -74,12 +86,25 @@ def parse_args():
         type=range_str,
         help="A time range (inclusive) of the form x,y where x and y are floats. Will only render elements between time [x, y] in the final animation.",
     )
+    parser.add_argument(
+        '-s',
+        '--stage',
+        type=ProcessStage,
+        choices=list(ProcessStage),
+        default=ProcessStage.timing,
+        help="Processing stage to stop after."
+    )
     args = parser.parse_args()
     if args.beat_range is not None and args.time_range is not None:
         # TODO: figure out how to print this nicer
         raise argparse.ArgumentError(
             argument=None,
             message="Cannot provide both --beat-range and --time-range arguments.",
+        )
+    if args.stage == ProcessStage.animate and args.harmonimation_file is None:
+        raise argparse.ArgumentError(
+            argument=hrmn_file_arg_def,
+            message=f"required for stage {ProcessStage.animate.name}"
         )
     return args
 
@@ -90,26 +115,31 @@ def main():
 
     # parse music data
     music_data = parse_score_data(args.musicxml_file.read())
-
-    # parse into timing data. (data, beat) -> (data, beat, second). filter if needed.
     if args.beat_range:
+        # filter by beat
         music_data = music_data.filter_by_beat_range(*args.beat_range)
+    if args.stage == ProcessStage.parse_score:
+        with open("music_data.json", 'w') as f:
+            f.write(music_data.export())
+        return
+
+    # parse into timing data (data, beat) -> (data, beat, second)
     resolve_timing(music_data)
     if args.time_range:
+        # filter by time
         # TODO: compensate for create time and start buffer time?
         music_data = music_data.filter_by_time_range(*args.time_range)
-    # print(music_data)
-
-    # export music_data
+    # always export at this stage
     with open("music_data.json", 'w') as f:
         f.write(music_data.export())
+    if args.stage == ProcessStage.timing:
+        return
 
     # make harmonimation widgets
     widgets = build_widgets(
         config=pyjson5.load(args.harmonimation_file),
         music_data=music_data,
     )
-
     # make harmonimation scene, and render!
     config.disable_caching = True # TODO: make config / cmd line argument
     GlassPanel(music_data, widgets).render()
