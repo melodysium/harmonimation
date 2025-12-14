e2101e581dca48d04c2f487fd21992cd1a18f1f1
melodysium <melodysium@gmail.com>
2025-12-13 22:40:23 -0500

    fix: disable TestCircle12Notes auto apply animations

--- godot-animator/src/component/test_circle_12_notes.gd
+++ godot-animator/src/component/test_circle_12_notes.gd
@@ -11,11 +11,12 @@ extends Node2D
 
 func _ready() -> void:
 	#print(animation_player.get_animation("play_music_circle"))
-	
-	## TODO: REMOVE. testing: call animate
-	if animation_player != null:
-		var animation := Utils.setup_animation(animation_player)
-		#Utils.apply_animation(circle_12_notes.animate_rotate(0, PI / 3, 2, 3), animation_player, animation)
-		
-		for anim in circle_12_notes.animate_on_data(Utils.MUSIC_DATA):
-			Utils.apply_animation(anim, animation_player, animation)
+
+	# TODO: REMOVE. testing: call animate
+	#if animation_player != null:
+		#var animation := Utils.setup_animation(animation_player)
+		##Utils.apply_animation(circle_12_notes.animate_rotate(0, PI / 3, 2, 3), animation_player, animation)
+		#
+		#for anim in circle_12_notes.animate_on_data(Utils.MUSIC_DATA):
+			#Utils.apply_animation(anim, animation_player, animation)
+	pass
