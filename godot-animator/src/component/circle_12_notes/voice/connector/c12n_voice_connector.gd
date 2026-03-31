511b16a942813ce84895329aaac8d212ab3d9529
Melody Griesen <Melody.Griesen@genesys.com>
2026-03-30 23:30:28 -0400

    feat: switch voice connectors to use NodeProvider

--- godot-animator/src/component/circle_12_notes/voice/connector/c12n_voice_connector.gd
+++ godot-animator/src/component/circle_12_notes/voice/connector/c12n_voice_connector.gd
@@ -54,6 +54,30 @@ var _c12n: Circle12Notes:
 ## List of line objects. created in animate_chord_roots
 var _lines: Array[LineState] = []
 
+## NodeProvider registration for re-used Line2D nodes
+var _line_registration: NodeProvider.NodeTypeRegistration = Utils.NODE_PROVIDER.register(
+	self.get_script().get_global_name(),
+	"Line2D",
+	_create_line,
+	func () -> void: pass,
+	{"default_color": Color.TRANSPARENT},
+)
+
+## unique ids used for re-used Line2Ds
+var _next_line_id := 0
+
+
+# Problem: how to initialize line position properly when activated?
+# options:
+# 1. set points in animation track. problem: will probably conflict with signals that update it too.
+# 2. Call Method track. problem: doesn't run in editor preview.
+# 3. animate a dummy property with setter which calls desired function. god i hate this
+var _line_state_trigger: LineState:
+	set(value):
+		print("calling _position_line from _line_state_trigger setter = %s" % [value])
+		_position_line(value)
+		print("done calling _position_line setter")
+
 #endregion
 
 
@@ -75,6 +99,23 @@ func _ready() -> void:
 
 #region Private helpers
 
+func _create_line(player: AnimationPlayer, animation: Animation) -> Line2D:
+	var new_line := Line2D.new()
+	new_line.default_color = Color.TRANSPARENT
+	new_line.width = 1
+	new_line.name = "C12NLine_%d" % [_next_line_id]
+	_next_line_id += 1
+	new_line.add_point(Vector2.ZERO)
+	new_line.add_point(Vector2.ZERO)
+	_c12n.add_child(new_line)
+
+	# Set a transparent keyframe at the very beginning for this line
+	var track_idx := Utils.find_or_make_track(player, animation, new_line, "default_color", Animation.TYPE_VALUE)
+	animation.track_insert_key(track_idx, 0, Color.TRANSPARENT, 0.0)
+
+	return new_line
+
+
 func _update_lines() -> void:
 	if not is_node_ready():
 		return
@@ -85,12 +126,21 @@ func _update_lines() -> void:
 
 
 func _position_line(line_state: LineState) -> void:
+	if line_state == null:
+		printerr("C12NVoiceConnector._position_line() called with null arg! skipping")
+		return
+	var arr := _compute_line_pos(line_state)
+	line_state.peek_line().points = arr
+
+
+func _compute_line_pos(line_state: LineState) -> PackedVector2Array:
 	var shorten_len := _c12n.radius * .21
 	var start_center_pos := _c12n._get_position_for_pitchclass(line_state.start_pitch)
 	var end_center_pos := _c12n._get_position_for_pitchclass(line_state.end_pitch)
 	var dir_vec := (end_center_pos - start_center_pos).normalized()
-	line_state.line.set_point_position(0, start_center_pos + dir_vec * shorten_len)
-	line_state.line.set_point_position(1, end_center_pos - dir_vec * shorten_len)
+	var line_start_pos := start_center_pos + dir_vec * shorten_len
+	var line_end_pos := end_center_pos - dir_vec * shorten_len
+	return PackedVector2Array([line_start_pos, line_end_pos])
 
 
 #endregion
@@ -117,8 +167,10 @@ func animate_chord_roots(chord_roots: Array[Dictionary]) -> Dictionary[Variant, 
 	print_verbose("C12NVoiceConnector.animate_chord_roots(): start")
 	var anims: Dictionary[Variant, Dictionary] = {}
 
+	anims[self] = {"_line_state_trigger": []}
+
 	var selected_pitches: Array[int] = []
-	var previous_line_states: Dictionary[Node, Color] = {} # TODO maybe expand Color into a full LineState?
+	var previous_line_states: Dictionary[NodeProvider.NodePromise, Color] = {} # TODO maybe expand Color into a full LineState?
 
 	for chord_root: Dictionary in chord_roots:
 		var time: float = chord_root["time"]
@@ -138,19 +190,13 @@ func animate_chord_roots(chord_roots: Array[Dictionary]) -> Dictionary[Variant, 
 		print_verbose("    new selected_pitches=%s, previous_pitch_class=%d" % [selected_pitches, previous_pitch_class])
 
 		# setup for adding a line
-		var new_line := Line2D.new()
-		new_line.default_color = Color.TRANSPARENT
-		new_line.width = 1
-		new_line.name = "C12NLine%d_%sto%s" % [_lines.size()+1,
-			Utils.Pitch.pitchclass_to_pitchname(previous_pitch_class),
-			Utils.Pitch.pitchclass_to_pitchname(newest_pitch_class)]
+		var new_line := _line_registration.request(time)
 		var line_state := LineState.create(previous_pitch_class, newest_pitch_class, new_line)
-		new_line.add_point(Vector2.ZERO)
-		new_line.add_point(Vector2.ZERO)
-		_position_line(line_state)
 		_lines.append(line_state)
-		_c12n.add_child(new_line)
-		anims[new_line] = {"default_color": [Utils.PropertyKeyframePoint.new(Color.TRANSPARENT, 0)]}
+		anims[new_line] = {"default_color": []}
+		#var line_pos := _compute_line_pos(line_state) # can't directly position line imperatively here, must be done via animated property
+		# this will cause the line position to be updated
+		Utils.as_array(anims[self]["_line_state_trigger"]).append(Utils.PropertyKeyframePoint.new(new_line, time, 0.0))
 		previous_line_states[new_line] = Color.TRANSPARENT
 
 		# setup for removing a line
@@ -160,9 +206,12 @@ func animate_chord_roots(chord_roots: Array[Dictionary]) -> Dictionary[Variant, 
 			print_verbose("    removing a line. _voice.max_active.v=%d, selected_pitches.size()=%d, _lines.size()=%d" % [_voice.max_active.v, selected_pitches.size(), _lines.size()])
 			# add PropertyChange for turning off this line
 			var old_line_idx := _lines.size() - selected_pitches.size()
-			var old_line := _lines[old_line_idx].line
+			var old_line := _lines[old_line_idx].line_promise
+			old_line.done(time) # tell the promise that this is when we're done with this line
 			Utils.as_array(anims[old_line]["default_color"]).append(Utils.PropertyKeyframePoint.new(Color.TRANSPARENT, time, 0.0, 0.1, -4.0))
-		
+			# add a trigger for positioning line here (in case user is scrubbing backwards on timeline)
+			Utils.as_array(anims[self]["_line_state_trigger"]).append(Utils.PropertyKeyframePoint.new(old_line, time + 0.001, 0.0))
+
 		print_verbose("    setting up for new selected_pitches: %s at time %ss" % [selected_pitches, time])
 
 		for i in range(selected_pitches.size() - 1):
@@ -170,11 +219,19 @@ func animate_chord_roots(chord_roots: Array[Dictionary]) -> Dictionary[Variant, 
 			#var color := Color(1.0, 1.0, 1.0, 0.6) ** i # doesn't work :(
 			var new_color := Color.WHITE
 			new_color.a = _voice.alpha_decay_factor ** (i+1)
-			var selected_line := _lines[line_idx].line
-			Utils.as_array(anims[selected_line]["default_color"]).append(Utils.PropertyKeyframePoint.new(new_color, time, 0.0, 0.1, -4.0))
+			var selected_line := _lines[line_idx].line_promise
 			print_verbose("      i=%d, line_idx=%d, previous_color=%s, new_color=%s" % [i, line_idx, previous_line_states[selected_line], new_color])
+			# print(anims)
+			# print(anims[selected_line])
+			Utils.as_array(anims[selected_line]["default_color"]).append(Utils.PropertyKeyframePoint.new(new_color, time, 0.0, 0.1, -4.0))
 			previous_line_states[selected_line] = new_color
-	
+
+	# set end_times for all remaining nodes
+	var end_time: float = chord_roots[-1]["time"] + 10.0
+	for i in range(selected_pitches.size()):
+		_lines[-i].line_promise.done(end_time)
+
+
 	print_verbose("C12NVoiceConnector.animate_chord_roots(): end")
 	return anims
 
--- godot-animator/src/component/circle_12_notes/voice/connector/line_state.gd
+++ godot-animator/src/component/circle_12_notes/voice/connector/line_state.gd
@@ -5,19 +5,22 @@ extends Object
 
 var start_pitch: int
 var end_pitch: int
-var line: Line2D
+var line_promise: NodeProvider.NodePromise # NodeProvider<Line2D>
 # TODO: length: float(0.0 - 1.0), from start to end
 
 static func create(
 	_start_pitch: int,
 	_end_pitch: int,
-	_line: Line2D
+	_line_promise: NodeProvider.NodePromise
 ) -> LineState:
 	var state := LineState.new()
 	state.start_pitch = _start_pitch
 	state.end_pitch = _end_pitch
-	state.line = _line
+	state.line_promise = _line_promise
 	return state
 
+func peek_line() -> Line2D:
+	return self.line_promise.peek()
+
 func _to_string() -> String:
-	return "LineState(start_pitch=%d, end_pitch=%d, line=%s)" % [start_pitch, end_pitch, line]
+	return "LineState(start_pitch=%d, end_pitch=%d, line_promise=%s)" % [start_pitch, end_pitch, line_promise]
